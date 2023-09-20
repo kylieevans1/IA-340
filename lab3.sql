10add92605b292bb902c6d59ded366023c47df31
kylieevans1 <143033811+kylieevans1@users.noreply.github.com>
2023-09-20 16:04:55 -0400

    Update lab3.sql

--- lab3.sql
+++ lab3.sql
@@ -12,7 +12,7 @@ values ('p1@jmu.edu','p1','o1'),
 
 --Q2.3
 insert into course(c_number,c_name,room,p_email)
-values('c1','postgressql','r1','p1@jmu.edu'),
+values ('c1','postgressql','r1','p1@jmu.edu'),
 ('c2','mongodb','r2','p2@jmu.edu'),
 ('c3','twitter','r1','p1@jmu.edu')
 
@@ -25,16 +25,15 @@ insert into enroll(s_email,c_number)
   ('s2@jmu.edu','c3'),
   ('s3@jmu.edu','c3')
   
---Q2.5 
-  /*insert professor first because you need the professor's information before the course 
-  
+--Q2.5 /* insert professor first because you need the professor's information before the course 
+        
   insert into professor(p_email,p_name,office)
   values('p3@jmu.edu','p3','o3');
 
   insert into course(c_number,c_name,room,p_email)
   values('c4','facebook','r1','p3@jmu.edu')
 
---Q2.6/* modify course first because you need to change the p_email in the course before you delete the professor's records
+--Q2.6 /* modify course first because you need to change the p_email in the course before you delete the professor's records
 
 update course
 set p_email = 'p3@jmu.edu'
@@ -43,6 +42,28 @@ where p_email = 'p1@jmu.edu'
 delete from professor 
 where p_email = 'p1@jmu.edu'
 
-  
+--Q2.7 
+select * from enroll 
+
+--Q2.8 
+select c_number, count(*) as num_student
+from enroll 
+group by c_number
+order by num_student desc
+limit 1
+
+--Q2.9
+select professor.p_name, course.c_name
+from professor 
+inner join course
+on professor.p_email = course.p_email
+
+--Q2.10
+select professor.p_name, count (course.c_number) as num_courses_taught
+from professor 
+inner join course on professor.p_email = course.p_email 
+group by professor.p_name
+order by num_courses_taught desc
+limit 1 
   
 
